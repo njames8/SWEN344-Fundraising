4422cdd1034e6d2c12410d57c861199afc02c1e0
NicholasJames <nicholas.james@wegmans.onmicrosoft.com>
2018-03-05 15:14:49 -0500

    changing to es6

--- routes/api/pendingCampaigns.js
+++ routes/api/pendingCampaigns.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var file = require("../../mock_data/pending_campaigns.json");
+let express = require('express');
+let router = express.Router();
+let file = require("../../mock_data/pending_campaigns.json");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.json(file);
