2beb2138a3f474e9f66a75f021cb22e28b4f8f8c
NicholasJames <nicholas.james@wegmans.onmicrosoft.com>
2018-04-27 09:38:59 -0400

    create script to create db schema

---
+++ database/createSchema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE [IF NOT EXISTS] [fundraising].user_info (
+    user_id     INT PRIMARY KEY,
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL,
+    email       TEXT NOT NULL,
+    balance     REAL DEFAULT 0,
+    is_admin    BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE [IF NOT EXISTS] [fundraising].campaign (
+    campaign_id INT PRIMARY KEY,
+    owner_id    INT NOT NULL,
+    title       TEXT NOT NULL,
+    description TEXT,
+    goal        REAL NOT NULL,
+    total       REAL DEFAULT 0,
+    start_date  DATE NOT NULL,
+    end_date    DATE NOT NULL,
+    image       TEXT,
+    is_live     BOOLEAN DEFAULT FALSE,
+    is_pending  BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (owner_id) REFERENCES user_info(user_id)
+);
+
+CREATE TABLE [IF NOT EXISTS] [fundraising].campaign_contributor (
+    campaign_contributor_id INT PRIMARY KEY,
+    campaign_id             INT NOT NULL,
+    user_id                 INT NOT NULL,
+    contribution            REAL NOT NULL,
+    FOREIGN KEY (campaign_id) REFERENCES campaign(campaign_id),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+);
