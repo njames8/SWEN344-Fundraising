2fdbf8b892552eec3aab482da97a54519cb2027b
atg5281 <atgaustin@gmail.com>
2018-04-29 22:00:58 -0400

    Login not working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render(__dirname  + '/index.html');
+    res.render(__dirname  + '/login.html');
 });
 
 module.exports = router;
